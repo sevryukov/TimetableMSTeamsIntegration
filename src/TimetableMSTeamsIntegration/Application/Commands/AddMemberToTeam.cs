541e22a1ffd78f71aec2e4f17cf154dceaa8a71a
Ilya Katun <hypnospinner@gmail.com>
2020-08-14 20:03:24 +0300

    issues/5 impl AddMemberToTeam command

--- src/TimetableMSTeamsIntegration/Application/Commands/AddMemberToTeam.cs
+++ src/TimetableMSTeamsIntegration/Application/Commands/AddMemberToTeam.cs
@@ -3,17 +3,24 @@ using System.Threading.Tasks;
 using MediatR;
 using TimetableMSTeamsIntegration.Application.Services;
 using Microsoft.Graph;
+using TimetableMSTeamsIntegration.Domain.Entities.Aggregates.TeamAggregates;
+using System;
 
 namespace TimetableMSTeamsIntegration.Application.Commands
 {
     public class AddMemberToTeam : IRequest
     {
-        // TODO: put here information about member and team
+        public AddMemberToTeam(Guid memberId, Guid teamId)
+        {
+            MemberId = memberId;
+            TeamId = teamId;
+        }
+        public Guid MemberId { get; private set; }
+        public Guid TeamId { get; private set; }
     }
 
     public class AddMemberToTeamHandler : IRequestHandler<AddMemberToTeam>
     {
-
         private readonly IIntegrationRepository _integrationRepository;
         private readonly IMSGraphClient _graphClient;
 
@@ -27,9 +34,29 @@ namespace TimetableMSTeamsIntegration.Application.Commands
 
         public async Task<Unit> Handle(AddMemberToTeam request, CancellationToken cancellationToken)
         {
-            // TODO: implement handling
-            // get info about what member (ms id) we want to add to which team (ms id) and post this info in graph
-            // post info about adding member to service db
+            try
+            {
+                // adding member to MS Team via Graph Client
+                await _graphClient.AddMemberAsync(
+                    memberId: request.MemberId,
+                    teamId: request.TeamId
+                );
+
+                // everything is ok => create event in repo that describes that member was added
+                await _integrationRepository.CreateAddMemberEventAsync(
+                    memberId: request.MemberId,
+                    teamId: request.TeamId
+                );
+            }
+            catch (Exception e)
+            {
+                // ? maybe we need some logging on errors in such situations and there we should put it, 
+                // ? via injected (in future) logging service 
+
+                throw e;
+            }
+
+            // ok, return
 
             return Unit.Value;
         }
