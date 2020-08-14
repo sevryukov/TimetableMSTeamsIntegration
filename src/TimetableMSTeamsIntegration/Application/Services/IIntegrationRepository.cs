d963fad81b428887d179da85ad9764dfb66df7eb
Ilya Katun <hypnospinner@gmail.com>
2020-08-14 20:03:00 +0300

    issues/5 partially described interfaces 4 services

--- src/TimetableMSTeamsIntegration/Application/Services/IIntegrationRepository.cs
+++ src/TimetableMSTeamsIntegration/Application/Services/IIntegrationRepository.cs
@@ -2,6 +2,10 @@ using System;
 using TimetableMSTeamsIntegration.Domain.Entities;
 using TimetableMSTeamsIntegration.Domain.Entities.Aggregates.TeamAggregates;
 using TimetableMSTeamsIntegration.Domain.Entities.Aggregates.MeetingAggregates;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TimetableMSTeamsIntegration.Domain.Abstract;
+
 namespace TimetableMSTeamsIntegration.Application.Services
 {
     public interface IIntegrationRepository
@@ -15,8 +19,8 @@ namespace TimetableMSTeamsIntegration.Application.Services
         void CloseTeam(TeamClosed teamClosedEvent);
         void CancelMeeting(MeetingCancelled meetingCancelledEvent);
         void FinishMeeting(MeetingFinished meetingFinishedEvent);
+        Task<IEnumerable<IAggregate<Team>>> GetTeamEventsAsync(Guid teamId);
         void RemoveMember(MemberRemoved memberRemovedEvent);
-
         void CreateTeam(TeamCreated teamCreatedEvent);
         void CreateMeeting(MeetingCreated meetingCreatedEvent);
         void AddMember(MemberAdded memberAddedEvent);
@@ -24,7 +28,10 @@ namespace TimetableMSTeamsIntegration.Application.Services
         void ShiftMeeting(MeetingShifted meetingShiftedEvent);
 
 
-
-
+        Task CreateAddMemberEventAsync(Guid memberId, Guid teamId);
+        Task CreateRangeAddMemberEventAsync(IEnumerable<(Guid memberId, Guid teamId)> events);
+        Task CreateRemoveMemberEventAsync(Guid memberId, Guid teamId);
+        Task CreateRangeRemoveMemberEventAsync(IEnumerable<(Guid memberId, Guid teamId)> events);
+        Task CreateCreateTeamEventAsync(Guid discipline, Guid division, Guid contingentUnit, int year, int semester);
     }
 }
--- src/TimetableMSTeamsIntegration/Application/Services/IMSGraphClient.cs
+++ src/TimetableMSTeamsIntegration/Application/Services/IMSGraphClient.cs
@@ -1,17 +1,21 @@
 using TimetableMSTeamsIntegration.Domain.Entities.Aggregates.TeamAggregates;
 using TimetableMSTeamsIntegration.Domain.Entities.Aggregates.MeetingAggregates;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
 namespace TimetableMSTeamsIntegration.Application.Services
 {
     public interface IMSGraphClient
     {
         void CreateTeam(TeamCreated teamCreatedEvent);
         void AddMember(MemberAdded memberAddedEvent);
+        Task AddMemberAsync(Guid memberId, Guid teamId);
         void RemoveMember(MemberRemoved memberRemovedEvent);
         void CreateMeeting(MeetingCreated meetingCreatedEvent);
         void CancelMeeting(MeetingCancelled meetingCancelledEvent);
         void FinishMeeting(MeetingFinished meetingFinishedEvent);
         void ShiftMeeting(MeetingShifted meetingShiftedEvent);
-        
-        
+        Task<Guid> CreateTeamAsync(Guid discipline, Guid division, Guid contingentUnit, int year, int semester, ICollection<Guid> members = null);
     }
 }
