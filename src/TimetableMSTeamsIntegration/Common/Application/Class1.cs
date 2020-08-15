6e33ae2a3dd59f24679564736acf24b2ebcf1daf
Ilya Katun <hypnospinner@gmail.com>
2020-08-15 19:15:43 +0300

    issues/5 add common projects for appl layer and infra layer

---
+++ src/TimetableMSTeamsIntegration/Common/Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TimetableMSTeamsIntegration.Common.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/TimetableMSTeamsIntegration/Common/Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TimetableMSTeamsIntegration.Common.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
