16f8da8bc6e45826d065cb599417ffdb73c3e5b3
Ilya Katun <hypnospinner@gmail.com>
2020-08-20 23:21:05 +0300

    issues/11 add common generic repository interface

--- src/TimetableMSTeamsIntegration/Common/Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimetableMSTeamsIntegration.Common.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/TimetableMSTeamsIntegration/Common/Application/Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TimetableMSTeamsIntegration.Common.Domain;
+
+namespace TimetableMSTeamsIntegration.Common.Application
+{
+    public interface IRepository<TEntity, TKey> where TEntity : IIdentifiable<TKey>
+    {
+        // CREATE
+        Task InsertAsync(TEntity entity);
+        Task InsertAsync(IEnumerable<TEntity> entities);
+
+        // READ
+        Task<TEntity> GetAsync(TKey id);
+        Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter);
+
+        // DELETE
+        Task DeleteAsync(TKey id);
+        Task DeleteAsync(Expression<Func<TEntity, bool>> filter);
+
+        // UPDATE
+        Task UpdateAsync(TEntity entity);
+        Task UpdateAsync(IEnumerable<TEntity> entities);
+    }
+}
